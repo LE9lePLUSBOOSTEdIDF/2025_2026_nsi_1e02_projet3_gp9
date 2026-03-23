09704a12d6f952ee80659d90bd18cdc1eeec98b1
stanigoat <stan.paturel@gmail.com>
2026-03-23 10:50:04 +0100

    ajout du fichier assets

--- stan_test_tuto.py
+++ stan_test_tuto.py
@@ -4,6 +4,20 @@ pygame.init() # Charger tous les modules de la bibliothèque
 # Création de la fenêtre
 
 pygame.display.set_caption("Jeu de NSI") # Création du titre
-pygame.display.set_mode(500, 300) # Dimensions de la fenêtre
+main_screen = pygame.display.set_mode((1080, 720)) # Dimensions de la fenêtre
 
-7:63 du tutoriel
+background = pygame.image.load("assets/background_castle.png") 
+
+game_running = True # Création de la boucle pour faire tourner le jeu en continu
+
+while game_running:
+
+    main_screen.blit(background, (0, 0)) # Ajout de l'arrière plan à l'écran
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            game_running = False
+            pygame.quit()
