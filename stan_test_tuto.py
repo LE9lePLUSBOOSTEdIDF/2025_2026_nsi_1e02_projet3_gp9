0dee024e211f584ee6e4e3ac3b2d424443e32b1b
stanigoat <stan.paturel@gmail.com>
2026-03-23 09:56:04 +0100

    Premières expériences

---
+++ stan_test_tuto.py
@@ -0,0 +1,9 @@
+import pygame
+pygame.init() # Charger tous les modules de la bibliothèque
+
+# Création de la fenêtre
+
+pygame.display.set_caption("Jeu de NSI") # Création du titre
+pygame.display.set_mode(500, 300) # Dimensions de la fenêtre
+
+7:63 du tutoriel
