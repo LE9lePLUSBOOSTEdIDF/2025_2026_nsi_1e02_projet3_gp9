c16904dd211fce3ddb258bf8273ba4b0d98a7636
staaaaaaaan <stan.paturel@gmail.com>
2026-03-25 20:52:05 +0100

    Poursuite du tutoriel

--- stan_test_tuto.py
+++ stan_test_tuto.py
@@ -1,28 +1,25 @@
 import pygame
+from game import *
+
 pygame.init() # Charger tous les modules de la bibliothèque
-
-class Player(pygame.sprite.Sprite):
-
-    def __init__(self):
-        self.health = 100
-        self.max_health = 100
-        self.attack = 10
-        self.velocity = 10
 
 # Création de la fenêtre
 
 pygame.display.set_caption("Jeu de NSI") # Création du titre
 main_screen = pygame.display.set_mode((1080, 720)) # Dimensions de la fenêtre
 
-background = pygame.image.load("assets/background_castle.png") 
+background = pygame.image.load("assets/background_castle.png")  # Chargement de l'arrière plan
 background = pygame.transform.scale(background, (1080, 720))
 
+game = Game()
 game_running = True # Création de la boucle pour faire tourner le jeu en continu
 
 while game_running:
 
-    main_screen.blit(background, (0, 50)) # Ajout de l'arrière plan à l'écran
-    pygame.display.flip()
+    main_screen.blit(background, (0, 0)) # Affichage l'arrière plan à l'écran
+    main_screen.blit(game.player.image, game.player.rect) # Affichage du sprite joueur
+
+    pygame.display.flip() # Actualisation de l'écran
 
     for event in pygame.event.get():
 
@@ -31,4 +28,4 @@ while game_running:
             game_running = False
             pygame.quit()
 
-Episode 2
+#Episode 2
