07af94ed5ca26148e4f41bb37884b2907ba95c0c
staaaaaaaan <stan.paturel@gmail.com>
2026-03-26 11:27:16 +0100

    Ajout de limite + réduction de la taille du spite

--- player.py
+++ player.py
@@ -4,14 +4,15 @@ class Player(pygame.sprite.Sprite): # Création de la classe joueur
 
     def __init__(self):
         super().__init__() # Association du joueur avec une image
+        self.sprite = pygame.image.load("assets/knight_sprite.png") # Chargement de l'image du joueur
         self.health = 100
         self.max_health = 100
         self.attack = 10
-        self.velocity = 10
-        self.image = pygame.image.load("assets/knight_sprite.png") # Chargement de l'image du joueur
+        self.velocity = 5
+        self.image = pygame.transform.scale_by(self.sprite, 0.35)
         self.rect = self.image.get_rect() # Récupération de la position du joueur
         self.rect.x = -50
-        self.rect.y = 100
+        self.rect.y = 400
 
     def move_right(self):
         self.rect.x += self.velocity
--- stan_test_tuto.py
+++ stan_test_tuto.py
@@ -19,10 +19,10 @@ while game_running:
     main_screen.blit(background, (0, 0)) # Affichage l'arrière plan à l'écran
     main_screen.blit(game.player.image, game.player.rect) # Affichage du sprite 
     
-    if game.pressed.get(pygame.K_d):
+    if game.pressed.get(pygame.K_d) and game.player.rect.x + game.player.rect.width < main_screen.get_width():
         game.player.move_right()
 
-    elif game.pressed.get(pygame.K_q):
+    elif game.pressed.get(pygame.K_q) and game.player.rect.x > 0:
         game.player.move_left()        
 
     pygame.display.flip() # Actualisation de l'écran
